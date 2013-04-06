2a429c231b183f9f2155afd7bc311be3f825db2e
Mao Chao <6233843@gmail.com>
2013-04-06 17:21:19 +0800

    [MC] add injector builder

---
+++ src/parser/InjectorBuilder.java
@@ -0,0 +1,43 @@
+package parser;
+
+import parser.result.InjectType;
+import parser.result.Injector;
+import parser.result.ReferenceType;
+
+public class InjectorBuilder {
+    Injector injector = new Injector();
+
+    public Injector build() {
+        return injector;
+    }
+
+    public InjectorBuilder name(String name) {
+        injector.setName(name);
+        return this;
+    }
+
+    public InjectorBuilder constructorInject() {
+        injector.setInjectType(InjectType.Constructor);
+        return this;
+    }
+
+    public InjectorBuilder value(Object value) {
+        injector.setValue(value);
+        return this;
+    }
+
+    public InjectorBuilder primitiveValue() {
+        injector.setIsReference(ReferenceType.Primitivity);
+        return this;
+    }
+
+    public InjectorBuilder setterInject() {
+        injector.setInjectType(InjectType.Setter);
+        return this;
+    }
+
+    public InjectorBuilder referenceValue() {
+        injector.setIsReference(ReferenceType.Reference);
+        return this;
+    }
+}
